c628d27bb2456044a74079b5f0f8a68e3f19ae38
MargaritaChoc7 <137134397+MargaritaChoc7@users.noreply.github.com>
2023-07-04 18:57:14 -0600

    Add files via upload

---
+++ Repaso del menu.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+
+int main () {
+	
+	while (a!=4){
+		cout<<"--MENÚ--"<<endl;
+		cout<<"1. ingresa el nombre y apellido del estudiante"<<endl;
+		cout<<"2. Ingrese las notas asignadas por el docente"<<endl;
+		cout<<"3. Visualizacion de las notas individuales"<<endl;
+		cout<<"4. Promedio total de las notas"<<endl;
+		cout<<"5. Salir"<<endl;
+		
+		cout<<"elige una opcion: "<<endl;
+		cin>> (opcion){
+			
+			case 1:
+				cout<<"ingrese nombre y apellido del estudiante"<<endl;
+				cin.ignore();
+				cin.getline(nombre,100);
+				break;
+				
+				case 2:
+				cout<<"Ingrese notas de los cursos asignados"<<endl;
+				 
+		}
+	}
+}
